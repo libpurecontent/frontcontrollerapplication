dd5b62d1ce7ac6f3c471b9d37a85794adf7d8a41
mvl22 <martin@lucas-smith.co.uk>
2009-10-27 18:25:38 +0000

    Version 1.2.17 (released 27/Oct/2009)
    * Fix for display of generic description if not logged in.

--- src/frontControllerApplication.php
+++ src/frontControllerApplication.php
@@ -5,7 +5,7 @@
 
 
 # Front Controller pattern application
-# Version 1.2.16
+# Version 1.2.17
 class frontControllerApplication
 {
  	# Define available actions; these should be extended by adding definitions in an overriden assignActions ()
@@ -268,7 +268,7 @@ class frontControllerApplication
 		# Require authentication for actions that require this
 		if (!$this->user && ((isSet ($this->actions[$this->action]['authentication']) && $this->actions[$this->action]['authentication']) || $this->settings['authentication'])) {
 			if ($this->settings['authentication']) {echo "\n<p>Welcome.</p>";}
-			echo "\n<p><strong>You need to " . ($this->settings['externalAuth'] ? "log in using [<a href=\"{$this->baseUrl}/login.html?{$location}\">Raven</a>] or [<a href=\"{$this->baseUrl}/loginexternal.html?{$location}\">Friends login</a>]" : "<a href=\"{$this->baseUrl}/login.html?{$location}\">log in (using Raven)</a>") . " before you can " . ($this->settings['authentication'] ? 'use this facility' : htmlspecialchars (strtolower (strip_tags ($this->actions[$this->action]['description'])))) . '.</strong></p>';
+			echo "\n<p><strong>You need to " . ($this->settings['externalAuth'] ? "log in using [<a href=\"{$this->baseUrl}/login.html?{$location}\">Raven</a>] or [<a href=\"{$this->baseUrl}/loginexternal.html?{$location}\">Friends login</a>]" : "<a href=\"{$this->baseUrl}/login.html?{$location}\">log in (using Raven)</a>") . " before you can " . ($this->actions[$this->action]['description'] ? htmlspecialchars (strtolower (strip_tags ($this->actions[$this->action]['description']))) : 'use this facility') . '.</strong></p>';
 			echo "\n<p>(<a href=\"{$this->baseUrl}/help.html\">Information on Raven accounts</a> is available.)</p>";
 			echo $endDiv;
 			return false;
