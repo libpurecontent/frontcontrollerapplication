b7667725d655e4a80f9198231896afc63a117c6d
mvl22 <martin@lucas-smith.co.uk>
2026-09-17 18:50:03 +0100

    Allow use of feedback page without auth, as the user may wish to ask about gaining access

--- src/frontControllerApplication.php
+++ src/frontControllerApplication.php
@@ -662,7 +662,7 @@ class frontControllerApplication
 		if (!$this->user && ($authRequiredByAction || $authRequiredGlobally)) {
 			
 			# Disable user authentication for pages never requiring this
-			$pagesNeverRequiringAuthentication = array ('register', 'resetpassword', 'help');
+			$pagesNeverRequiringAuthentication = array ('register', 'resetpassword', 'help', 'feedback');
 			if ($this->settings['dataDisableAuth']) {$pagesNeverRequiringAuthentication[] = 'data';}
 			if ($this->settings['apiUsername']) {$pagesNeverRequiringAuthentication[] = 'api';}
 			if (!in_array ($this->action, $pagesNeverRequiringAuthentication)) {
